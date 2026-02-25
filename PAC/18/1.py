51b1b8a63210111ef1a80bbec0dc3ddfb5516559
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-02-26 01:35:45 +0700

    PAC

--- PAC/18/1.py
+++ PAC/18/1.py
@@ -78,8 +78,8 @@ class SiameseNet(nn.Module):
         super().__init__()
         self.base_model = base_model
         self.classifier = nn.Sequential(
-            nn.Linear(128, 1),  # Выход - 1 число
-            nn.Sigmoid()  # Превращаем в вероятность от 0 до 1
+            nn.Linear(128, 1),
+            nn.Sigmoid()
         )
 
     def forward(self,x1,x2):
@@ -104,12 +104,12 @@ class Contrasitive(nn.Module):
         distance = F.pairwise_distance(x1,x2)
         loss = (1-label)*torch.pow(distance,2)+ label*torch.pow(torch.clamp(self.margin-distance,min=0),2)
 
-        loss_correct = label * torch.pow(distance, 2) + \
-                       (1 - label) * torch.pow(torch.clamp(self.margin - distance, min=0), 2)
-        return loss_correct.mean()
+        # loss_correct = label * torch.pow(distance, 2) + \
+        #                (1 - label) * torch.pow(torch.clamp(self.margin - distance, min=0), 2)
+        return loss.mean()
 
-# criterion = Contrasitive(1)
-criterion = nn.BCELoss()
+criterion = Contrasitive(1)
+# criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(siamese.parameters(), lr=0.0005)
 
 for epoch in range(20):
@@ -120,8 +120,10 @@ for epoch in range(20):
     for img1, img2, label in dataloader:
         optimizer.zero_grad()
         # print(label)
-        out = siamese(img1, img2)
-        loss = criterion(out, label.unsqueeze(1))
+        # out = siamese(img1, img2)
+        out1 = siamese.base_model(img1)
+        out2 = siamese.base_model(img2)
+        loss = criterion(out1,out2, label.unsqueeze(1))
         loss.backward()
         optimizer.step()
 
