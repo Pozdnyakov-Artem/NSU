71c5d54d19198d07e26600b32a69a3913acf0a8e
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-03-04 02:11:32 +0700

    PAC

--- PAC/19/1.py
+++ PAC/19/1.py
@@ -93,7 +93,7 @@ def main():
 
     model.train()
 
-    for epoch in range(10):
+    for epoch in range(20):
         num_batch = 0
         train_loss = 0
         for x_train, y_train in train_loader:
